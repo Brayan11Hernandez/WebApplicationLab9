5dfb115573269229de625353683d8f66464f4930
Brayan11 <Brayan11@LAB4-22>
2024-04-22 09:21:39 -0600

    Agregar archivos de proyecto.

---
+++ Reporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationLab9
+{
+    public class Reporte
+    {
+        public string nombreAlumno { get; set; }
+        public string nombreTaller { get; set; }
+
+    }
+}
